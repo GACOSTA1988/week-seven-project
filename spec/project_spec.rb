69e171905a1870ac9026cbed63668e299c22fee2
George Acosta <georgeacostar@gmail.com>
2019-12-13 11:16:46 -0800

    tdd spec 2 test passl

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -10,7 +10,7 @@ describe('Anagramer#anagram_checker') do
 end
 
 describe('Anagramer#anagram_checker') do
-  anagram_checker_test2 = Anagramer.new("rock","Raok")
+  anagram_checker_test2 = Anagramer.new("rock","Rock")
   it('account for the possibility that words might have different cases but should still be anagrams') do
     expect(anagram_checker_test2.anagram_checker).to(eq("These words are anagrams"))
   end
