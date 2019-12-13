fbbe5ff140a52313efadc100a9ae85ddda4554de
George Acosta <georgeacostar@gmail.com>
2019-12-13 09:33:52 -0800

    add def anagram checker

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Anagramer
+  def initialize(input_word_1, input_word_2)
+    @input_word_1 = input_word_1
+    @input_word_2 = input_word_2
+  end
+  
+  def anagram_checker
+    if input_word_1 == input_word_2
+      message = "These words are anagrams"
+    end
+    message
+  end
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,9 +3,9 @@ require('project.rb')
 require 'pry'
 
 describe('') do
-  test1 = Anagramer.new("rock","rack")
+  anagram_checker_test1 = Anagramer.new("rock","rack")
   it('should test to see if the words are anagrams') do
-    expect(test1.game_manager).to(eq("These words are anagrams"))
+    expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
   end
 end
 
