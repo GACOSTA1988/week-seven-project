4638fb45e10e4117a00b916cfddcb4cdebc67483
George Acosta <georgeacostar@gmail.com>
2019-12-13 09:59:29 -0800

    tdd spec 1 passl

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,7 +3,7 @@ require ('project.rb')
 require 'pry'
 
 describe('Anagramer#anagram_checker') do
-  anagram_checker_test1 = Anagramer.new("rock","rack")
+  anagram_checker_test1 = Anagramer.new("rock","rock")
   it('should test to see if the words are anagrams') do
     expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
   end
