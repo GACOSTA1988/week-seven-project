b97754610359083e27a30d7819b6270c2fe853c7
George Acosta <georgeacostar@gmail.com>
2019-12-13 10:36:17 -0800

    add tdd spec 2 test

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,3 +8,10 @@ describe('Anagramer#anagram_checker') do
     expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
   end
 end
+
+describe('Anagramer#anagram_checker') do
+  anagram_checker_test2 = Anagramer.new("rock","Rock")
+  it('account for the possibility that words might have different cases but should still be anagrams') do
+    expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
+  end
+end
