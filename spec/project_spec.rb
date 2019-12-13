1b9c4459c190c1766ab2e017a0f3d635c73c65c5
George Acosta <georgeacostar@gmail.com>
2019-12-13 09:11:32 -0800

    add test1

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,3 +1,11 @@
 require('rspec')
 require('project.rb')
+require 'pry'
+
+describe('') do
+  test1 = Anagramer.new("rock","rack")
+  it('should test to see if the words are anagrams') do
+    expect(test1.game_manager).to(eq("These words are anagrams"))
+  end
+end
 
