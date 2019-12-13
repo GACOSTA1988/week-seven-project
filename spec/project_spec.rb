631da42e853a2430babaea47d5d2e68d1bd15e07
George Acosta <georgeacostar@gmail.com>
2019-12-13 15:21:23 -0800

    troubleshoot

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,29 +2,29 @@ require ('rspec')
 require ('project.rb')
 require 'pry'
 
-describe('Anagramer#anagram_checker') do
-  anagram_checker_test1 = Anagramer.new("rock","rock")
-  it('should test to see if the words are anagrams') do
-    expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
-  end
-end
+# describe('Anagramer#anagram_checker') do
+#   anagram_checker_test1 = Anagramer.new("rock","rock")
+#   it('should test to see if the words are anagrams') do
+#     expect(anagram_checker_test1.anagram_checker).to(eq("These words are anagrams"))
+#   end
+# end
+
+# describe('Anagramer#anagram_checker') do
+#   anagram_checker_test2 = Anagramer.new("rock","Rock")
+#   it('account for the possibility that words might have different cases but should still be anagrams') do
+#     expect(anagram_checker_test2.anagram_checker).to(eq("These words are anagrams"))
+#   end
+# end
+
+# describe('Anagramer#anagram_checker') do
+#   anagram_checker_test3 = Anagramer.new("eat","tea")
+#   it('should test to see if the words are anagrams when letters are sorted different ') do
+#     expect(anagram_checker_test3.anagram_checker).to(eq("These words are anagrams"))
+#   end
+# end
 
 describe('Anagramer#anagram_checker') do
-  anagram_checker_test2 = Anagramer.new("rock","Rock")
-  it('account for the possibility that words might have different cases but should still be anagrams') do
-    expect(anagram_checker_test2.anagram_checker).to(eq("These words are anagrams"))
-  end
-end
-
-describe('Anagramer#anagram_checker') do
-  anagram_checker_test3 = Anagramer.new("eat","tea")
-  it('should test to see if the words are anagrams when letters are sorted different ') do
-    expect(anagram_checker_test3.anagram_checker).to(eq("These words are anagrams"))
-  end
-end
-
-describe('Anagramer#anagram_checker') do
-  anagram_checker_test4 = Anagramer.new("kk","rage")
+  anagram_checker_test4 = Anagramer.new("kkkk","kkk")
   it('should test to see if the inputed words contain a vowel') do
     expect(anagram_checker_test4.anagram_checker).to(eq("Sorry but all entry words must contain a vowel, please try again"))
   end
