7c4aece9ea5290bac1ea6d100759908e38c9a788
George Acosta <georgeacostar@gmail.com>
2019-12-15 16:36:00 -0800

    test scenarios for words containing vowels

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,28 @@
 class Anagramer
   def initialize(input_word_1, input_word_2)
-    @input_word_1 = input_word_1
-    @input_word_2 = input_word_2
+    @input_word_1 = input_word_1.upcase
+    @input_word_2 = input_word_2.upcase
+    @inputs = [@input_word_1, @input_word_2]
   end
 
   
+  # THIS WILL RETURN TRUE IF WORD HAS VOWELS
+def are_both_words 
+  are_words = @inputs.select {|input| input.match(/[AEIOU]/) }
+  
+  if are_words.length == 2
+   return "both are words"
+  end
+
+  if are_words.length == 1
+    return "only one word is valid"
+  end
+
+  if are_words.length == 0
+   return "neither of these are words"
+  end
+end
+
   def anagram_checker
     word_1_split = @input_word_1.upcase.split('').sort_by {|word_1_split| word_1_split }
     word_1_vowels = @input_word_1.scan(/\w[AEIOUaeiou]/) 
@@ -14,20 +32,28 @@ class Anagramer
 
     puts word_1_vowels.length == 0 
     puts word_2_vowels.length == 0 
-    # word_2_vowels.length == 0 
+    # word_2_vowels.length == 0
+    # binding.pry
     if word_1_vowels.length == 0 
-    binding.pry
     message = "Sorry but all entry words must contain a vowel, please try again"
-    end
-    if word_2_vowels.length == 0 
+    
+    elsif word_2_vowels.length == 0 
     message = "Sorry but all entry words must contain a vowel, please try again"
-    binding.pry
-    end
-    if word_1_split == word_2_split
+    
+    elsif word_1_split == word_2_split
       message = "These words are anagrams"
+
     else
-      messege = "These words are not anagrams"
+      message = "These words are not anagrams"
     end
-   
+    message
+  end
+end
+
+def is_antigram(array1, array2)
+  array1.each do |letter|
+    return true if array2.includes?(letter)
+
+    return false
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -23,9 +23,29 @@ require 'pry'
 #   end
 # end
 
-describe('Anagramer#anagram_checker') do
-  anagram_checker_test4 = Anagramer.new("kkkaak","kkk")
-  it('should test to see if the inputed words contain a vowel') do
-    expect(anagram_checker_test4.anagram_checker).to(eq("Sorry but all entry words must contain a vowel, please try again"))
+# describe('Anagramer#anagram_checker') do
+#   anagram_checker_test4 = Anagramer.new("kkakk","kkak")
+#   it('should test to see if the inputed words contain a vowel') do
+#     expect(anagram_checker_test4.anagram_checker).to(eq("Sorry but all entry words must contain a vowel, please try again"))
+#   end
+
+#   it('should test to see if the inputed words contain a vowel') do
+#     expect(anagram_checker_test4.anagram_checker).to(eq("Sorry but all entry words must contain a vowel, please try again"))
+#   end
+# end
+describe('Anagramer#are_both_words') do
+  it('returns "both are words" if both words are valid') do
+    two_real_words = Anagramer.new("cat","bed")
+    expect(two_real_words.are_both_words).to(eq("both are words"))
+  end
+
+  it('returns  "only one word is valid" if both words are valid') do
+    one_real_words = Anagramer.new("ct","bed")
+    expect(one_real_words.are_both_words).to(eq( "only one word is valid"))
+  end
+
+  it('returns "neither of these are words" if both words are valid') do
+    no_real_words = Anagramer.new("ct","bd")
+    expect(no_real_words.are_both_words).to(eq("neither of these are words"))
   end
 end
